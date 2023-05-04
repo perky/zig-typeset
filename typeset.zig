a7683ad672b7d9bf63ac9c2b4a62882b7568c271
Luke <luke.perkin@gmail.com>
2023-05-04 21:44:00 +0100

    zig fmt typeset.zig

--- typeset.zig
+++ typeset.zig
@@ -42,8 +42,8 @@
 //!     pub const debug_name = <user defined debug name>;
 //!
 //!     pub const init = fn (data: anytype) T;
-//!     pub const call = fn (data_ptr: anytype, 
-//!                     comptime fn_name: []const u8, 
+//!     pub const call = fn (data_ptr: anytype,
+//!                     comptime fn_name: []const u8,
 //!                     args: anytype) <return type of method>;
 //!     pub const Tag = fn (comptime SubT: type) tag_type;
 //!     pub const get = fn (comptime SubT: type, data: anytype) SubT;
@@ -59,27 +59,24 @@ pub fn Typeset(comptime name: []const u8, comptime types: anytype) type {
     inline for (types, 0..) |T, i| {
         enum_fields[i] = .{ .name = @typeName(T), .value = i };
     }
-    const TypesetTag = @Type(.{
-        .Enum = .{
-            .tag_type = meta.Int(.unsigned, math.log2_int_ceil(u16, types.len)),
-            .fields = &enum_fields,
-            .decls = &[_]builtin.Type.Declaration{},
-            .is_exhaustive = true,
-        }
-    });
+    const TypesetTag = @Type(.{ .Enum = .{
+        .tag_type = meta.Int(.unsigned, math.log2_int_ceil(u16, types.len)),
+        .fields = &enum_fields,
+        .decls = &[_]builtin.Type.Declaration{},
+        .is_exhaustive = true,
+    } });
     var union_fields: [types.len]builtin.Type.UnionField = undefined;
     inline for (types, 0..) |T, i| {
         union_fields[i] = .{ .name = @typeName(T), .type = T, .alignment = @alignOf(T) };
     }
-    const U = @Type(.{
-        .Union = .{
-            .layout = .Auto,
-            .tag_type = TypesetTag,
-            .fields = &union_fields,
-            .decls = &[_]builtin.Type.Declaration{}
-        }
-    });
-
+    // zig fmt: off
+    const U = @Type(.{ .Union = .{ 
+        .layout = .Auto, 
+        .tag_type = TypesetTag, 
+        .fields = &union_fields, 
+        .decls = &[_]builtin.Type.Declaration{} 
+    } });
+    // zig fmt: on
     return struct {
         pub const T = U;
         pub const tag_type = TypesetTag;
@@ -89,24 +86,21 @@ pub fn Typeset(comptime name: []const u8, comptime types: anytype) type {
             return @unionInit(U, @typeName(@TypeOf(data)), data);
         }
 
-        pub fn call(data_ptr: anytype, 
-                    comptime fn_name: []const u8, 
-                    args: anytype) ReturnTypeForMethod(U, fn_name, name) 
-        {
-             return switch(data_ptr.*) {
+        pub fn call(data_ptr: anytype, comptime fn_name: []const u8, args: anytype) ReturnType(U, fn_name, name) {
+            return switch (data_ptr.*) {
                 inline else => |*val_ptr| blk: {
-                    const _T = @TypeOf(val_ptr.*);
-                    switch(@typeInfo(_T)) {
+                    const val_type = @TypeOf(val_ptr.*);
+                    switch (@typeInfo(val_type)) {
                         .Pointer => |type_info| {
                             const fn_ptr = &@field(type_info.child, fn_name);
                             break :blk @call(.auto, fn_ptr, .{val_ptr.*} ++ args);
                         },
                         else => {
-                            const fn_ptr = &@field(_T, fn_name);
+                            const fn_ptr = &@field(val_type, fn_name);
                             break :blk @call(.auto, fn_ptr, .{val_ptr} ++ args);
-                        }
+                        },
                     }
-                }
+                },
             };
         }
 
@@ -120,65 +114,55 @@ pub fn Typeset(comptime name: []const u8, comptime types: anytype) type {
     };
 }
 
-fn ReturnTypeForMethod(comptime U: type, 
-                       comptime fn_name: []const u8,
-                       comptime typeset_name: []const u8) type 
-{
-    switch(@typeInfo(U)) {
-        .Union => |info| {
-            var found_type: ?type = null;
-            inline for (info.fields) |field_info| {
-                const T = field_info.type;
-                switch (@typeInfo(T)) {
-                    .Struct, .Union, .Enum, .Opaque => {
-                        if (@hasDecl(T, fn_name)) {
-                            const fn_t = @field(T, fn_name);
-                            found_type = @typeInfo(@TypeOf(fn_t)).Fn.return_type.?;
-                            break;
-                        }
-                    },
-                    .Pointer => |ptr| {
-                        const T2 = ptr.child;
-                        if (@hasDecl(T2, fn_name)) {
-                            const fn_t = @field(T2, fn_name);
-                            found_type = @typeInfo(@TypeOf(fn_t)).Fn.return_type.?;
-                            break;
-                        }
-                    },
-                    else => unreachable
-                }
-            }
-            if (found_type) |T| {
-                return T;
-            } else {
-                @compileError("method named " ++ fn_name ++ " not found in any types of Typeset " ++ typeset_name);
-            }
-        },
-        else => unreachable
+fn ReturnType(comptime U: type, comptime fn_name: []const u8, comptime typeset_name: []const u8) type {
+    const type_info = @typeInfo(U);
+    if (type_info != .Union) {
+        @compileError("Expected " ++ @typeName(U) ++ " to be a union.");
     }
+
+    inline for (type_info.Union.fields) |field_info| {
+        const T = field_info.type;
+        switch (@typeInfo(T)) {
+            .Struct, .Union, .Enum, .Opaque => {
+                if (@hasDecl(T, fn_name)) {
+                    const fn_t = @field(T, fn_name);
+                    return @typeInfo(@TypeOf(fn_t)).Fn.return_type.?;
+                }
+            },
+            .Pointer => |ptr| {
+                const T2 = ptr.child;
+                if (@hasDecl(T2, fn_name)) {
+                    const fn_t = @field(T2, fn_name);
+                    return @typeInfo(@TypeOf(fn_t)).Fn.return_type.?;
+                }
+            },
+            else => @compileError("Invalid type for Typeset method call: " ++ @typeName(T)),
+        }
+    }
+    @compileError("Method named " ++ fn_name ++ " not found in any types of Typeset " ++ typeset_name);
 }
 
 const tests = struct {
     pub const testing = std.testing;
-    const Orc = struct { 
-        hp: u32 = 10, 
-        pub fn heal(self: *@This(), amount: u32) void { 
+    const Orc = struct {
+        hp: u32 = 10,
+        pub fn heal(self: *@This(), amount: u32) void {
             self.hp += amount;
         }
-        pub fn getHealth(self: *const @This()) u32 { 
+        pub fn getHealth(self: *const @This()) u32 {
             return self.hp;
         }
     };
-    const Troll = struct { 
-        hp: u32 = 50, 
-        pub fn heal(self: *@This(), amount: u32) void { 
-            self.hp += amount*2; 
+    const Troll = struct {
+        hp: u32 = 50,
+        pub fn heal(self: *@This(), amount: u32) void {
+            self.hp += amount * 2;
         }
-        pub fn getHealth(self: *const @This()) u32 { 
+        pub fn getHealth(self: *const @This()) u32 {
             return self.hp;
         }
     };
-    const Monster = Typeset("monster", .{Orc, Troll});
+    const Monster = Typeset("monster", .{ Orc, Troll });
     test "typeset init" {
         var monster = Monster.init(Troll{});
         try testing.expectEqualStrings("monster", Monster.debug_name);
@@ -208,7 +192,7 @@ const tests = struct {
         try testing.expectEqual(@as(u32, 10), hp2);
     }
     test "typeset pointers mix" {
-        const MonsterWithPtr = Typeset("Monster2", .{Orc, *Troll});
+        const MonsterWithPtr = Typeset("Monster2", .{ Orc, *Troll });
         var troll = Troll{ .hp = 100 };
         var monster1 = MonsterWithPtr.init(&troll);
         try testing.expectEqual(@as(u32, 100), Monster.get(*Troll, monster1).*.hp);
@@ -221,4 +205,6 @@ const tests = struct {
         try testing.expectEqual(@as(u32, 51), Monster.get(Orc, monster2).hp);
     }
 };
-test { std.testing.refAllDecls(tests); }
+test {
+    std.testing.refAllDecls(tests);
+}
